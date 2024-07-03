131e539966ceaf1c8a79720ba72084f757a19d5e
onegqueen <rkdwldnjs30@naver.com>
2024-07-03 13:08:38 +0900

    feat : 사용자의 가장 최근 게시글을 조회하는 레포지토리 메소드

--- turntable/src/main/java/com/example/turntable/repository/DailyCommentRepository.java
+++ turntable/src/main/java/com/example/turntable/repository/DailyCommentRepository.java
@@ -1,10 +1,12 @@
 package com.example.turntable.repository;
 
 import com.example.turntable.domain.DailyComment;
+import java.time.LocalDateTime;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DailyCommentRepository  extends JpaRepository<DailyComment, Long> {
-
+    Optional<DailyComment> findFirstByMember_IdOrderByCreatedAtDesc(long memberId);
 }
